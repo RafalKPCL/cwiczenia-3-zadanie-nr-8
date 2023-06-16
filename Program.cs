270d367bb524c4e588f52c7def029beac047402d
RafalKPCL <chx100524@student.chorzow.wsb.pl>
2023-06-16 02:49:13 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Pomyśl o liczbie z przedziału 1-20.");
+
+        int min = 1;
+        int max = 20;
+        int guess;
+        char answer;
+
+        do
+        {
+            guess = (min + max) / 2;
+
+            Console.WriteLine($"Czy pomyślana liczba to {guess}? Odpowiedz ujemną (<), dodatnią (>) lub zero (=).");
+            answer = Console.ReadKey().KeyChar;
+
+            if (answer == '<')
+            {
+                max = guess - 1;
+            }
+            else if (answer == '>')
+            {
+                min = guess + 1;
+            }
+
+        } while (answer != '=');
+
+        Console.WriteLine($"\nOdgadłem! Pomyślana liczba to {guess}.");
+
+        Console.ReadKey();
+    }
+}
